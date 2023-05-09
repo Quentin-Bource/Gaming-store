d135be9c3ef02f298e7f17206f1b987d7c1d79e9
Quentin <quentin01.bource@gmail.com>
2023-05-09 12:23:28 +0200

    faker ok

--- Game/resources/views/partials/nav.blade.php
+++ Game/resources/views/partials/nav.blade.php
@@ -1,7 +1,7 @@
 <div class="bg-slate-800 w-full  opacity-100 text-slate-50 inline-flex rounded-t-md">
 
-    <a class="w-48" href="{{ route('home') }}"><img class="w-full" src="./img/logo.png" alt="Logo"></a>
-<ul class="inline-flex items-center ml-40  text-xl" >
+    <a class="" href="{{ route('home') }}"><img class=" w-48 mr-10" src="./img/logo.png" alt="Logo"></a>
+<ul class="inline-flex items-center ml-30  text-xl" >
     <li class="m-10 hover:text-violet-500"><a href="{{ route('home') }}">Home</a></li>
     <li class="m-10 hover:text-violet-500"><a href="{{ route('game') }}">Games</a></li>
     <li class="m-10 hover:text-violet-500"><a href="{{ route('about') }}">About</a></li>
